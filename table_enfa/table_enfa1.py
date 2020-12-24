b048602dfc6a1d9fbc9335f5661626a0b32a8bc0
liunix <liuyifan.com@gmail.com>
2020-12-24 18:50:32 +0800

    print simple reg-enfa to table

--- table_enfa/table_enfa1.py
+++ table_enfa/table_enfa1.py
@@ -1,53 +1,70 @@
 """ 
 正则表达式制作-enfa表
-TODO:
-1. 格式化输出
-2. 理解enfa运行原理
+FIXME:
+1. 目前只是单状态制表
+2. 字母表为{a,b}
 """
-
-class State:
+from box import Box
+class NFA_State:
     global_id = 0
     def __init__(self, is_end: bool = False):
         self.is_end = is_end
         self.transition = {}
         self.epsilon_transition = []
 
-        self.id = State.global_id
-        State.global_id += 1
+        # self.id = NFA_State.global_id
+        self.id = 'q'+str(NFA_State.global_id)
+        NFA_State.global_id += 1
+
 
     def __str__(self):
         output = ""
-        output += "State {}{}\n".format(self.id, " (END)" if self.is_end else "")
-        output += "Transition:\n"
-        output += str(["State {}, t={}".format(i.id, t) for t, i in self.transition.items()]) + "\n"
-        output += "Epsilon transition:\n"
-        output += str(["State {}".format(i.id) for i in self.epsilon_transition]) + "\n"
-        output += "-----------------------\n"
+        a_target =Box({'id':"ϕ"})
+        b_target =Box({'id':"ϕ"})
+        ε_target =Box({'id':"ϕ"})
+        
+        for transition_char, target_state in self.transition.items():
+            if transition_char == 'a':
+                a_target = target_state
+            if transition_char == 'b':
+                b_target = target_state
+        for epsilon_target_state in self.epsilon_transition:
+            ε_target = epsilon_target_state 
+
+
+        output = "{}\t\t{}\t\t{}\t\t{}\n".format(self.id,a_target.id,b_target.id,ε_target.id) #NOTE:这里可能触发递归
+        # print("output=".id,output,"\n")
         return output
 
 
+
 class NFA:
-    def __init__(self, start: State, end: State):
+    def __init__(self, start: NFA_State, end: NFA_State):
         self.start = start
         self.end = end
 
     def __str__(self):
-        def all_transitions(come_from: State) -> [State]:
-            output = list(come_from.transition.values())
-            output.extend(come_from.epsilon_transition)
+        def all_transitions(from_state):
+            output = list(from_state.transition.values())
+            output.extend(from_state.epsilon_transition)
+            # print("all_transitions=",str(output))
             return output
 
-        # Perform depth-first search from given state, print them.
-        def traverse_state(stand_on: State) -> str:
+        # 深度优先遍历
+        def traverse_state(current_state):
+            # 忽略 已经访问过的state
             output = ""
-            if id(stand_on) in visited_id:
+            if id(current_state) in visited_id:
                 return output
 
-            output = str(stand_on)
-            visited_id.add(id(stand_on))
             
-            for t in all_transitions(stand_on):
-                output += traverse_state(t)
+            output = str(current_state)
+            visited_id.add(id(current_state))
+            
+            merged_transitions_target_state = all_transitions(current_state)
+
+            for transition_target_state_item in merged_transitions_target_state:
+                output += traverse_state(transition_target_state_item)
 
             return output
 
@@ -92,24 +109,24 @@ def to_NFA(postfix_exp: str) -> NFA:
     return stack.pop()
 
 # 状态转移1: epsilon 连接
-def add_epsilon_transition(come_from: State, to: State) -> None:
+def add_epsilon_transition(come_from: NFA_State, to: NFA_State) -> None:
     come_from.epsilon_transition.append(to)
 
 # 状态转移2: 字母符号 连接 
-def add_transition(come_from: State, to: State, symbol: str) -> None:
+def add_transition(come_from: NFA_State, to: NFA_State, symbol: str) -> None:
     come_from.transition[symbol] = to
 
 # 基本1: From epsilon.
 def from_epsilon() -> NFA:
-    start = State(is_end=False)
-    end = State(is_end=True)
+    start = NFA_State(is_end=False)
+    end = NFA_State(is_end=True)
     add_epsilon_transition(start, end)
     return NFA(start, end)
 
 # 基本2: From a single symbol.
 def from_symbol(symbol: str) -> NFA:
-    start = State(is_end=False)
-    end = State(is_end=True)
+    start = NFA_State(is_end=False)
+    end = NFA_State(is_end=True)
     add_transition(start, end, symbol)
     return NFA(start, end)
 
@@ -121,11 +138,11 @@ def concat(first: NFA, second: NFA) -> NFA:
 
 # 组合2. 合并 a|b -> ab|
 def union(first: NFA, second: NFA) -> NFA:
-    start = State(is_end=False)
+    start = NFA_State(is_end=False)
     add_epsilon_transition(start, first.start)
     add_epsilon_transition(start, second.start)
 
-    end = State(is_end=True)
+    end = NFA_State(is_end=True)
     add_epsilon_transition(first.end, end)
     first.end.is_end = False
     add_epsilon_transition(second.end, end)
@@ -135,8 +152,8 @@ def union(first: NFA, second: NFA) -> NFA:
 
 # 组合3. 闭包 a* -> a*
 def closure(nfa: NFA) -> NFA:
-    start = State(is_end=False)
-    end = State(is_end=True)
+    start = NFA_State(is_end=False)
+    end = NFA_State(is_end=True)
     
     add_epsilon_transition(start, end)
     add_epsilon_transition(start, nfa.start)
@@ -163,7 +180,8 @@ class Test_NFA(unittest.TestCase):
 
     # TODO: 简化输出，制成表格
     def test_concat(self):
-        nfa = to_NFA("ab.")
+        nfa = to_NFA("ab.*") #NOTE: 这里要是postfix表达式
+        print("<>\t\ta\t\tb\t\tε")
         print(nfa)
 
 if __name__ == "__main__":
