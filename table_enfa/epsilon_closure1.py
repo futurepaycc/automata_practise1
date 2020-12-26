6facdb758c6d09b51e5cf635ec8d02e71fadac19
liunix <liuyifan.com@gmail.com>
2020-12-26 21:35:43 +0800

    add some reference

--- table_enfa/epsilon_closure1.py
+++ table_enfa/epsilon_closure1.py
@@ -1,3 +1,10 @@
+""" 
+来源: https://stackoverflow.com/questions/49226978/recursion-over-a-dictionary-for-epsilon-closure-stops-working-when-list-is-passe
+将tf2中的list改成了tuple，否则出错
+还可参考:
+https://github.com/nvaneethm/eclosure/blob/master/eclosure.ipynb
+"""
+
 tf2 = {('q0','a') : 'q1', ('q1','eps') : ('q2', 'q3'), ('q3','eps') : 'q0', ('q2','a') : 'q4', ('q4','eps') : 'q3'}
 
 def epsilonClosure(transition, state):
