bc7155a88b8672e0ddb9605773b5f2472db76eaf
liunix <liuyifan.com@gmail.com>
2022-08-29 11:51:43 +0800

    tidy code

--- lr_test/lr0_closure2.py
+++ lr_test/lr0_closure2.py
@@ -26,28 +26,39 @@ G2 = [
 CURSOR_SYM = "∙"
 nterms = ["S'","S","B"]
 terms  = ["a","b"]
-# ---------------------------test2: 尝试嵌套字典存储, 可以整体判断相等性
-
-
-def recClosure(rules,next_sym,res):
+# ------------------------------------------------------------------------p1: 递归计算closure并inplace更新到res中
+def closure(rules,next_sym,res):
     for rule in rules:
         left,right = rule["left"],rule["right"]
         if left == next_sym:
             res.append(  {"left":left,"right":[CURSOR_SYM] + right} )
             # 如果右侧首是非终结，要递归拿到所有
             if right[0] in nterms:
-                recClosure(rules,right,res)
+                closure(rules,right[0],res)
 
+# ------------------------------------------------------------------------p2: 计算初始状态
 def build_st0(rules):
     first_rule = rules[0]
     left0,right0 = first_rule["left"],first_rule["right"]
     res =  [{"left":left0,"right":[CURSOR_SYM] + right0}] 
     next_sym = right0[0]
-    recClosure(rules[1:],next_sym,res)
+    closure(rules[1:],next_sym,res)
     return res
 
 all_sts = {}
 st0 = build_st0(G2)
+
+def _test_build_st0():
+    st0_hand = [
+        {"left":"S'","right":["∙","S"]},
+        {"left":"S","right":["∙","B","B"]},
+        {"left":"B","right":["∙","a","B"]},
+        {"left":"B","right":["∙","b"]},
+    ]
+    res = build_st0(G2)
+    assert( st0_hand == res )
+_test_build_st0()
+# ------------------------------------------------------------------------p3: 由一个状态项出发，推导出新状态
 all_sts[0] = st0
 current_sts_idx = 0
 
@@ -78,20 +89,18 @@ def gen_all_sts(rules,st0):
                 # 下一个终结符: 和上面结束一样直接添加为新状态
                 # 下一个非终结符: 递归找左部
                 if next_sym in nterms:
-                    recClosure(rules[1:],next_sym,res)
+                    closure(rules[1:],next_sym,res)
             check_add_st(res)
 
-# ---------------------------------------- 计算项目集规范族
+# ------------------------------------------------------------------------p4: 推导至 项目集规范族 不再变化为止
 gen_all_sts(G2,st0)
-prev_lenth = 1
-changed:bool = True
-while changed:
+prev_lenth = 1  #从初始状态第2项开始往下
+while True:
     new_length = len(all_sts.keys())
     if new_length > prev_lenth:
         for key in range(prev_lenth,new_length):
             gen_all_sts(G2,all_sts[key])
         prev_lenth = new_length
     else:
-        changed = False
-
+        break
 pprint(all_sts)
